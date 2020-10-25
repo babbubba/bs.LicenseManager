ccce94c457dc66d360832782eb19a7a0d9ca425a
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2020-10-25 10:49:17 +0100

    Unit test created

---
+++ bs.LicenseManager.Test/LicenseServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bs.LicensesManager.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using bs.Data;
+using bs.Data.Interfaces;
+using bs.LicensesManager.Core.Repository;
+using System.Threading.Tasks;
+
+namespace bs.LicensesManager.Core.Service.Tests
+{
+    [TestClass()]
+    public class LicenseServiceTests
+    {
+        private IServiceProvider serviceProvider;
+        private IServiceCollection services;
+
+        [TestInitialize]
+        public void Init()
+        {
+            services = new ServiceCollection();
+            services.AddBsData(new DbContext
+            {
+                ConnectionString = "Data Source=.\\bs.LicenseManager.Test.db;Version=3;BinaryGuid=False;",
+                DatabaseEngineType = DbType.SQLite,
+                Create = true,
+                Update = true,
+                LookForEntitiesDllInCurrentDirectoryToo = false
+            });
+            services.AddScoped<LicenseRepository>();
+            services.AddScoped<LicenseService>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+
+        [TestMethod()]
+        public void CreateCustomerTest()
+        {
+            var licenseService = serviceProvider.GetService<LicenseService>();
+            var customerId = licenseService.CreateCustomer("bSoft", "fcavallari@bsoftsolutions.it");
+        }
+
+        [TestMethod()]
+        public void CompleteWorkFlowTest()
+        {
+            Task.Run(async () => {
+                var licenseService = serviceProvider.GetService<LicenseService>();
+                var customerId = await licenseService.CreateCustomer("bSoft", "fcavallari@bsoftsolutions.it");
+                var productId = await licenseService.CreateProduct("License Manager", "Applicazione per la gestione delle licenze");
+                var versionId = await licenseService.CreateVersion(productId, "Versione 1.0.0.0");
+                var feature1Id = await licenseService.CreateProductFeature(productId, "ModuleOne");
+                var feature2Id = await licenseService.CreateProductFeature(productId, "ModuleTwo");
+                var licenseTokenId = await licenseService.CreateLicense(versionId, customerId, DateTime.UtcNow.AddDays(30), Standard.Licensing.LicenseType.Standard);
+                var feature1ValueId = licenseService.CreateLicenseTokenFeature(licenseTokenId, feature1Id, "true");
+                var feature2ValueId = licenseService.CreateLicenseTokenFeature(licenseTokenId, feature2Id, "false");
+                await licenseService.EmitLicense(licenseTokenId);
+
+            }).Wait();
+           
+        }
+    }
+}
+
+
---
+++ bs.LicenseManager/Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using bs.LicensesManager.Core.Model;
+using bs.LicensesManager.Core.Service;
+using bs.LicensesManager.Core.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bs.LicencesManager.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly LicenseService licenseService;
+
+        public CustomerController(IMapper mapper,LicenseService licenseService)
+        {
+            this.mapper = mapper;
+            this.licenseService = licenseService;
+        }
+        // GET: Customer
+        public async Task<ActionResult> Index()
+        {
+           return View(await licenseService.GetCustomersView());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CustomerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var customerId = await licenseService.CreateCustomer(model.Name, model.EmailContact);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        // GET: Customer/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            return View(await licenseService.GetCustomer(id));
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(CustomerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await licenseService.UpdateCustomer(model.Id, model.Name, model.EmailContact, model.Active);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
